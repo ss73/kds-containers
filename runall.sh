82b2047773d50e8d5656f69ca10cdda8ad59975b
ss73 <stellan.soderstrom@gmail.com>
2016-10-16 18:46:20 +0200

    Removed port mapping except for web app

--- runall.sh
+++ runall.sh
@@ -1,5 +1,6 @@
-docker run -d --name indexsvc --link solr -p 32600:32600 index_svc:1.0
-docker run -d --name blobsvc -p 32500:32500 blob_svc:1.0
-docker run -d --name convsvc -p 32400:32400 convert_svc:1.0
-docker run -d --name webapp --link indexsvc --link blobsvc -p 32000:32000 es_webapp:1.0
-docker run -d --name batchsvc --link indexsvc --link blobsvc --link webapp --link convsvc -p 32700:32700  batch_svc:1.0
+#!/bin/sh
+docker run -d --name indexsvc --link solr index_svc:1.0
+docker run -d --name blobsvc blob_svc:1.0
+docker run -d --name convsvc convert_svc:1.0
+docker run -d --name webapp --link indexsvc --link blobsvc -p 8080:32000 es_webapp:1.0
+docker run -d --name batchsvc --link indexsvc --link blobsvc --link webapp --link convsvc batch_svc:1.0
