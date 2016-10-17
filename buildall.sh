291b126554723ec217d6b4a9ad3b5be66173ede4
Stellan Söderström <stellan.soderstrom@gmail.com>
2016-10-17 16:46:12 +0200

    cleanup

--- buildall.sh
+++ buildall.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 cd $(dirname "$0")
-<<<<<<< HEAD
 echo "\n\n\n############################################\n#"
 echo "#   Building Solr"
 echo "#\n############################################\n"
@@ -24,17 +23,4 @@ echo "#\n############################################\n"
 echo "\n\n\n############################################\n#"
 echo "#  Buildning Batch Service"
 echo "#\n############################################\n"
-=======
-echo "Building Solr"
-./solr/build.sh
-echo "Building Index service"
-./indexsvc/build.sh
-echo "Building Blob Service"
-./blobsvc/build.sh
-echo "Building Web Application"
-./webapp/build.sh
-echo "Building Convert Service"
-./convertsvc/build.sh
-echo "Building Batch Service"
->>>>>>> 5bbe681f4730e939e892c644719449e8b620dcd7
 ./batchsvc/build.sh
