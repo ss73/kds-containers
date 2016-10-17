a961c7435709b28548fac5edf377e93c57fa47a1
Stellan Söderström <stellan.soderstrom@gmail.com>
2016-10-17 16:56:55 +0200

    Changed port for webapp to 8080

--- webapp/prodrun.sh
+++ webapp/prodrun.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d --name webapp --link indexsvc --link blobsvc -p 80:32000 es_webapp:1.0 
+docker run -d --name webapp --link indexsvc --link blobsvc -p 8080:32000 es_webapp:1.0 
